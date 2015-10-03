1539bbc38d7e928e785d1b88ad6c6556c6bb666a
tongerpeng <437866961@qq.com>
2015-10-03 22:55:24 +0800

    correct the old file

--- 75.java
+++ 75.java
@@ -1,15 +1,51 @@
 public class Solution {
-    public void sortColors(int[] nums) {
-        int N = nums.length;
-        int temp=0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N-1-i; j++){
-                if(nums[j] > nums[j+1]){
-                   temp = nums[j];
-                   nums[j] = nums[j+1];
-                   nums[j+1] = temp;
-                }
-            }
-        }
-    }
+    
+	public void sortColors(int[] nums) {
+       
+	       if(nums == null || nums.length == 0 || nums.length == 1)return;
+
+       	       int n = nums.length;
+
+      	       int left = 0;//left指向了第一个1的位置
+
+      	       int right = n - 1;//right指向了右边第一个非2的位置
+
+      	       int i=0;
+
+      	       while(i <= right){
+
+           		if(nums[i] == 0){
+
+               			swap(nums,left,i);
+
+               			left++;
+
+               			i++;
+
+           		}
+
+          		 else if(nums[i] == 1)i++;
+
+          		 else if(nums[i] == 2){
+
+              		 	swap(nums,i,right);
+
+              		 	right--;
+
+           		}
+
+       		}
+
+    	}
+    
+	public void swap(int[] nums,int i,int j){
+
+        
+        int temp = nums[i];
+
+        	nums[i] = nums[j];
+
+        	nums[j] = temp;
+
+    	}
 }
